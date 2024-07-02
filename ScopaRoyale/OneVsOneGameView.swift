d27e02a3ce93ef785cbe9dfba8f2dbbda8099d28
NickColella02 <nicolo.colella001@studenti.uniparthenope.it>
2024-07-02 21:22:58 +0200

    Va a fa e bukkin
    Commit muto.

--- ScopaRoyale/OneVsOneGameView.swift
+++ ScopaRoyale/OneVsOneGameView.swift
@@ -8,10 +8,10 @@ struct OneVsOneGameView: View {
         return scene
     }
     
-    @EnvironmentObject private var peerManager: MultiPeerManager // Accesso al MultiPeerManager dall'ambiente
+    @EnvironmentObject private var peerManager: MultiPeerManager
     @Environment(\.presentationMode) var presentationMode
     @State private var backModality = false
-    @State private var showPeerDisconnectedAlert = false // Variabile di stato per l'alert
+    @State private var showPeerDisconnectedAlert = false
     @State private var draggedCard: Card? = nil
     @State private var cardOffset: CGSize = .zero
 
@@ -23,7 +23,6 @@ struct OneVsOneGameView: View {
             
             VStack {
                 if peerManager.isHost {
-                    // Sezione per le carte dell'avversario, posizionate sopra il tavolo se sei l'host
                     VStack {
                         HStack(spacing: 4) {
                             ZStack {
@@ -36,10 +35,11 @@ struct OneVsOneGameView: View {
                                         .background(Color.white)
                                         .clipShape(RoundedRectangle(cornerRadius: 2))
                                         .shadow(radius: 3)
-                                        .rotationEffect(index == 0 ? Angle(degrees: 10) : (index == 2 ? Angle(degrees: -10) : Angle(degrees: 0)))
+                                        .rotationEffect(peerManager.opponentHand.count >= 3 ? (index == 0 ? Angle(degrees: 10) : (index == 2 ? Angle(degrees: -10) : Angle(degrees: 0))) : Angle(degrees: 0))
                                         .rotationEffect(.degrees(180))
                                         .zIndex(index == 1 ? 1 : 0)
-                                        .offset(x: index == 0 ? -20 : (index == 2 ? 20 : 0))
+                                        .offset(x: peerManager.opponentHand.count >= 3 ? (index == 0 ? -20 : (index == 2 ? 20 : 0)) : 0)
+                                        .offset(y: peerManager.opponentHand.count < 3 ? -10 : 0) // Aggiunta l'offset verticale
                                 }
                             }
                         }
@@ -60,21 +60,22 @@ struct OneVsOneGameView: View {
                                         .background(Color.white)
                                         .clipShape(RoundedRectangle(cornerRadius: 2))
                                         .shadow(radius: 3)
-                                        .rotationEffect(index == 0 ? Angle(degrees: 20) : (index == 2 ? Angle(degrees: -20) : Angle(degrees: 0)))
+                                        .rotationEffect(peerManager.playerHand.count >= 3 ? (index == 0 ? Angle(degrees: 20) : (index == 2 ? Angle(degrees: -20) : Angle(degrees: 0))) : Angle(degrees: 0))
                                         .rotationEffect(.degrees(180))
                                         .zIndex(index == 1 ? 1 : 0)
-                                        .offset(x: index == 0 ? -24 : (index == 2 ? 24 : 0))
+                                        .offset(x: peerManager.playerHand.count >= 3 ? (index == 0 ? -24 : (index == 2 ? 24 : 0)) : 0)
+                                        .offset(y: peerManager.playerHand.count < 3 ? -10 : 0) // Aggiunta l'offset verticale
                                 }
                             }
                         }
                         .padding(.horizontal)
-                        .padding(.bottom, 10) // Reduced bottom padding
+                        .padding(.bottom, 10)
                     }
                 }
                 
                 Spacer()
                 
-                // Sezione per le carte del tavolo, posizionate al centro
+                // Sezione per le carte sul tavolo
                 LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 4), spacing: 20) {
                     ForEach(peerManager.tableCards, id: \.self) { card in
                         Image(card.imageName)
@@ -93,109 +94,90 @@ struct OneVsOneGameView: View {
                 Spacer()
                 
                 if peerManager.isHost {
-                    // Sezione per le carte del giocatore (solo per l'host)
+                    // Sezione per le carte dell'host
                     VStack {
                         HStack(spacing: 4) {
-                            ForEach(peerManager.playerHand, id: \.self) { card in
-                                Image(card.imageName)
+                            ForEach(peerManager.playerHand.indices, id: \.self) { index in
+                                let card = peerManager.playerHand[index] // Salva la carta in una costante
+                                
+                                Image(card.imageName) // Utilizza direttamente la proprietà imageName della carta
                                     .resizable()
                                     .aspectRatio(contentMode: .fit)
                                     .frame(width: 75)
                                     .padding(2)
                                     .background(Color.white)
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                    .shadow(radius: 5)
-                                    .offset(card == draggedCard ? cardOffset : .zero)
                                     .clipShape(RoundedRectangle(cornerRadius: 5))
                                     .shadow(radius: 3)
+                                    .rotationEffect(peerManager.playerHand.count >= 3 ? (index == 0 ? Angle(degrees: -10) : (index == 2 ? Angle(degrees: 10) : Angle(degrees: 0))) : Angle(degrees: 0))
+                                    .zIndex(index == 1 ? 1 : 0)
+                                    .offset(x: peerManager.playerHand.count >= 3 ? (index == 0 ? 20 : (index == 2 ? -20 : 0)) : 0, y: peerManager.playerHand.count < 3 ? -10 : (index == 1 ? -10 : 0))
+                                    .offset(card == draggedCard ? cardOffset : .zero)
                                     .gesture(
                                         DragGesture()
                                             .onChanged { gesture in
-                                                draggedCard = card
+                                                draggedCard = card // Assegna direttamente la carta trascinata
                                                 cardOffset = gesture.translation
                                             }
                                             .onEnded { gesture in
-                                                if gesture.translation.height < -50 && peerManager.currentPlayer == 0 {
-                                                    peerManager.playCard(card: card)
+                                                if gesture.translation.height < 400 && peerManager.currentPlayer == 0 {
+                                                    peerManager.playCard(card: card) // Gioca la carta intera
                                                 }
-                                                // Resettare le variabili di stato dopo il rilascio del dito
                                                 draggedCard = nil
                                                 cardOffset = .zero
                                             }
                                     )
                                     .disabled(peerManager.currentPlayer != 0)
-                                }
-                        }
-                        .padding(.horizontal)
-                        .padding(.bottom, 150)
-                    }
-                }
-                if peerManager.isClient {
-                    // Sezione per le carte dell'avversario (solo per il client)
-                    VStack {
-                        HStack {
-                            ForEach(peerManager.opponentHand, id: \.self) { card in
-                                Image(card.imageName)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 60, height: 90)
-                                    .padding(4)
-                                    .background(Color.white)
-                                    .clipShape(RoundedRectangle(cornerRadius: 8))
-                                    .shadow(radius: 5)
-                                    .offset(card == draggedCard ? cardOffset : .zero)
-                                    .gesture(
-                                        DragGesture()
-                                            .onChanged { gesture in
-                                                draggedCard = card
-                                                cardOffset = gesture.translation
-                                            }
-                                            .onEnded { gesture in
-                                                if gesture.translation.height < -50 && peerManager.currentPlayer == 1 {
-                                                    peerManager.playCard(card: card)
-                                                }
-                                                // Resettare le variabili di stato dopo il rilascio del dito
-                                                draggedCard = nil
-                                                cardOffset = .zero
-                                            }
-                                    )
-                                    .disabled(peerManager.currentPlayer != 1)
-                        HStack(spacing: 20) {
-                            ZStack {
-                                ForEach(0..<peerManager.opponentHand.count, id: \.self) { index in
-                                    Image(peerManager.opponentHand[index].imageName)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 75)
-                                        .padding(2)
-                                        .background(Color.white)
-                                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                                        .shadow(radius: 3)
-                                        .gesture(
-                                            DragGesture()
-                                                .onEnded { gesture in
-                                                    if gesture.translation.height < -50 && peerManager.currentPlayer == 1 {
-                                                        peerManager.playCard(card: peerManager.opponentHand[index])
-                                                    }
-                                                }
-                                        )
-                                        .disabled(peerManager.currentPlayer != 1)
-                                        .rotationEffect(index == 0 ? Angle(degrees: -10) : (index == 2 ? Angle(degrees: 10) : Angle(degrees: 0)))
-                                        .zIndex(index == 1 ? 1 : 0)
-                                        .offset(x: index == 0 ? -60 : (index == 2 ? 60 : 0))
-                                }
                             }
                         }
                         .padding(.horizontal)
                         .padding(.bottom, 150)
                     }
                 }
-
+                if peerManager.isClient {
+                    // Sezione per le carte del client
+                    VStack {
+                        HStack {
+                            ForEach(peerManager.opponentHand.indices, id: \.self) { index in
+                                let card = peerManager.opponentHand[index] // Salva la carta in una costante
+                                
+                                Image(card.imageName) // Utilizza direttamente la proprietà imageName della carta
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 75)
+                                    .padding(2)
+                                    .background(Color.white)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .shadow(radius: 3)
+                                    .rotationEffect(peerManager.opponentHand.count >= 3 ? (index == 0 ? Angle(degrees: -10) : (index == 2 ? Angle(degrees: 10) : Angle(degrees: 0))) : Angle(degrees: 0))
+                                    .zIndex(index == 1 ? 1 : 0)
+                                    .offset(x: peerManager.opponentHand.count >= 3 ? (index == 0 ? 20 : (index == 2 ? -20 : 0)) : 0, y: peerManager.opponentHand.count < 3 ? -10 : (index == 1 ? -10 : 0))
+                                    .offset(card == draggedCard ? cardOffset : .zero)
+                                    .gesture(
+                                        DragGesture()
+                                            .onChanged { gesture in
+                                                draggedCard = card // Assegna direttamente la carta trascinata
+                                                cardOffset = gesture.translation
+                                            }
+                                            .onEnded { gesture in
+                                                if gesture.translation.height < 400 && peerManager.currentPlayer == 1 {
+                                                    peerManager.playCard(card: card) // Gioca la carta intera
+                                                }
+                                                draggedCard = nil
+                                                cardOffset = .zero
+                                            }
+                                    )
+                                    .disabled(peerManager.currentPlayer != 1)
+                            }
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 150)
+                    }
+                }
             }
         }
         .alert(isPresented: $showPeerDisconnectedAlert) {
             Alert(title: Text("Disconnessione"), message: Text("Il giocatore ha abbandonato la partita."), dismissButton: .default(Text("OK")) {
-                peerManager.reset() // chiude la connessione alla lobby quando uno dei due giocatori si disconnette
+                peerManager.reset()
                 backModality = true
             })
         }
@@ -211,6 +193,7 @@ struct OneVsOneGameView: View {
             }
         }
     }
+
     private func chunkedArray<T>(array: [T], chunkSize: Int) -> [[T]] {
         return stride(from: 0, to: array.count, by: chunkSize).map {
             Array(array[$0 ..< Swift.min($0 + chunkSize, array.count)])
